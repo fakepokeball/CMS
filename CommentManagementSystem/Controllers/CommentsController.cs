68252b2a6a5b215d03dd3e021d494272d675e2d9
Güven Çabat <gvncbt@gmail.com>
2024-07-11 16:28:32 +0300

    CommentsController

---
+++ CommentManagementSystem/Controllers/CommentsController.cs
@@ -0,0 +1,63 @@
+﻿using Business.Abstract;
+using Business.Requests.Comment;
+using Business.Responses.Comment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommentManagementSystem.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    private readonly ICommentService _commentService;
+
+    public CommentsController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpGet]
+    public GetCommentListResponse GetList([FromQuery] GetCommentListRequest request)
+    {
+        GetCommentListResponse response = _commentService.GetList(request);
+        return response;
+    }
+
+    [HttpGet("{Id}")]
+    public GetCommentByIdResponse GetById([FromRoute] GetCommentByIdRequest request)
+    {
+        GetCommentByIdResponse response = _commentService.GetById(request);
+        return response;
+    }
+
+    [HttpPost]
+    public ActionResult<AddCommentResponse> Add(AddCommentRequest request)
+    {
+        AddCommentResponse response = _commentService.Add(request);
+        return CreatedAtAction(
+            actionName: nameof(GetById),
+            routeValues: new { Id = response.Id },
+            value: response
+        );
+    }
+
+    [HttpPut("{Id}")]
+    public ActionResult<UpdateCommentResponse> Update(
+        [FromRoute] Guid Id,
+        [FromBody] UpdateCommentRequest request
+    )
+    {
+        if (Id != request.Id)
+            return BadRequest();
+
+        UpdateCommentResponse response = _commentService.Update(request);
+        return Ok(response);
+    }
+
+    [HttpDelete("{Id}")]
+    public DeleteCommentResponse Delete([FromRoute] DeleteCommentRequest request)
+    {
+        DeleteCommentResponse response = _commentService.Delete(request);
+        return response;
+    }
+}
--- CommentManagementSystem/Controllers/UsersController.cs
+++ CommentManagementSystem/Controllers/UsersController.cs
@@ -3,63 +3,62 @@ using Business.Requests.User;
 using Business.Responses.User;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CommentManagementSystem.Controllers
+namespace CommentManagementSystem.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
     {
+        _userService = userService;
+    }
 
-        private readonly IUserService _userService;
+    [HttpGet]
+    public GetUserListResponse GetList([FromQuery] GetUserListRequest request)
+    {
+        GetUserListResponse response = _userService.GetList(request);
+        return response;
+    }
 
-        public UsersController(IUserService userService)
-        {
-            _userService = userService;
-        }
+    [HttpGet("{Id}")]
+    public GetUserByIdResponse GetById([FromRoute] GetUserByIdRequest request)
+    {
+        GetUserByIdResponse response = _userService.GetById(request);
+        return response;
+    }
 
-        [HttpGet]
-        public GetUserListResponse GetList([FromQuery] GetUserListRequest request)
-        {
-            GetUserListResponse response = _userService.GetList(request);
-            return response;
-        }
+    [HttpPost]
+    public ActionResult<AddUserResponse> Add(AddUserRequest request)
+    {
+        AddUserResponse response = _userService.Add(request);
+        return CreatedAtAction(
+            actionName: nameof(GetById),
+            routeValues: new { Id = response.Id },
+            value: response
+        );
+    }
 
-        [HttpGet("{Id}")]
-        public GetUserByIdResponse GetById([FromRoute] GetUserByIdRequest request)
-        {
-            GetUserByIdResponse response = _userService.GetById(request);
-            return response;
-        }
+    [HttpPut("{Id}")]
+    public ActionResult<UpdateUserResponse> Update(
+        [FromRoute] Guid Id,
+        [FromBody] UpdateUserRequest request
+    )
+    {
+        if (Id != request.Id)
+            return BadRequest();
 
-        [HttpPost]
-        public ActionResult<AddUserResponse> Add(AddUserRequest request)
-        {
-            AddUserResponse response = _userService.Add(request);
-            return CreatedAtAction(
-                actionName: nameof(GetById),
-                routeValues: new { Id = response.Id },
-                value: response
-            );
-        }
+        UpdateUserResponse response = _userService.Update(request);
+        return Ok(response);
+    }
 
-        [HttpPut("{Id}")]
-        public ActionResult<UpdateUserResponse> Update(
-            [FromRoute] Guid Id,
-            [FromBody] UpdateUserRequest request
-        )
-        {
-            if (Id != request.Id)
-                return BadRequest();
-
-            UpdateUserResponse response = _userService.Update(request);
-            return Ok(response);
-        }
-
-        [HttpDelete("{Id}")]
-        public DeleteUserResponse Delete([FromRoute] DeleteUserRequest request)
-        {
-            DeleteUserResponse response = _userService.Delete(request);
-            return response;
-        }
+    [HttpDelete("{Id}")]
+    public DeleteUserResponse Delete([FromRoute] DeleteUserRequest request)
+    {
+        DeleteUserResponse response = _userService.Delete(request);
+        return response;
     }
 }
